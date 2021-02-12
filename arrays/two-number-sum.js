24cddf6457c6d26e8bfb4b47e2aaa67dc9379de1
Damla Köksal <me@damlakoksal.com>
2021-02-12 11:54:02 +0300

    fix: change array numbers in the example

--- arrays/two-number-sum.js
+++ arrays/two-number-sum.js
@@ -1,11 +1,11 @@
-function twoNumberSum(array, targetSum) {
+const twoNumberSum = (array, targetSum) => {
     const nums = {};
 
     for(const num of array) {
         // iterating elements of array individually
         const potentialMatch = targetSum - num; // looking for potential value next to num
         if(potentialMatch in nums) { // 
-            return [potentialMatch, num];
+            return [potentialMatch, num]; // there will be at most one pair
         }
         else {
             nums[num] = true;
@@ -14,6 +14,6 @@ function twoNumberSum(array, targetSum) {
     return [];
 }
 
-const array = [1, 2, 3, 8, 21, 5];
+const array = [1, 2, 3, 8, -6, 21, 5];
 const targetSum = 10;
 console.log(twoNumberSum(array, targetSum));
